21dddc6e86ef5dc1c7db47bf116403aa4d7a4b19
Aneesh Raman <aneeshraman97@gmail.com>
2019-11-21 14:53:42 -0500

    partial comment

---
+++ src/model/GradeEntry.java
@@ -0,0 +1,13 @@
+package model;
+
+public class GradeEntry {
+
+	private int id; 
+	private String entryName; 
+	private int itemId; 
+	private int categoryId; 
+	private double maxPoint; 
+	private double pointsEarned; 
+	private int courseId; 
+
+}
