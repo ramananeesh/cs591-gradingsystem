c71e72ba916949398f1526db6d3b66c8f39b48cc
Aneesh Raman <aneeshraman97@gmail.com>
2019-11-21 14:16:36 -0500

    Model class add

---
+++ src/model/Person.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Person {
+	private String name;
+
+	public Person(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	} 
+	
+	public String toString() {
+		return name.toString();
+	}
+}
