a6579886b45f2ff39414a4012c7dd4a9eab057f9
Aneesh Raman <aneeshraman97@gmail.com>
2019-11-26 10:36:18 -0500

    Updated controllers

--- src/controller/Master.java
+++ src/controller/Master.java
@@ -5,5 +5,30 @@ import model.*;
 
 public class Master {
 
-	private ArrayList<GenericCourse> courses; 
+	private ArrayList<Course> courses; 
+	
+	public Master() {
+		this.courses = new ArrayList<Course>();	
+	}
+	
+	public Master(ArrayList<Course> courses) {
+		this.courses = courses; 
+	}
+	
+	/*
+	 * Create Methods
+	 */
+	//-------------------------------------------------------------------------------------------------------------------------------------//
+	
+	public void addNewCourse(int courseId, String courseName, String term) {
+		Course newCourse = new Course(courseId, courseName, term);
+		this.courses.add(newCourse);
+	}
+	
+	public void addNewCourse(int courseId, String courseName, String term, ArrayList<Category> categories, ArrayList<CourseStudent> students) {
+		Course newCourse = new Course(courseId, courseName, term, categories, students);
+		this.courses.add(newCourse);
+	}
+	
+	
 }
