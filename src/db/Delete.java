b92520f4dcca2b3ac710a659231982fc7693dd2a
jakex49 <jakex49@gmail.com>
2019-12-03 18:13:37 -0500

    just commiting to push

---
+++ src/db/Delete.java
@@ -0,0 +1,9 @@
+package db;
+import model.*;
+
+
+public class Delete {
+    public static boolean removeCourseStudentFromCourse(String BUID, int courseID){
+        String sql = "Delete from CourseStudent"
+    }
+}
