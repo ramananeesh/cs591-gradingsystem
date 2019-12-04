47322fd2c4114bc81e32b0049ce5fe591b5b1890
Aneesh Raman <aneeshraman97@gmail.com>
2019-12-03 19:09:27 -0500

    one more change

--- src/view/GradePanel.java
+++ src/view/GradePanel.java
@@ -131,8 +131,8 @@ public class GradePanel extends JPanel {
 		// grade options combo
 		String[] gradeOptionsItems = { "Points Lost", "Percentage" };
 		gradeOptionsComboBox = new JComboBox<>(gradeOptionsItems);
-		gradeOptionsComboBox.setBounds(SizeManager.searchCourseBounds);
-		gradeOptionsComboBox.setFont(FontManager.fontSearch);
+		gradeOptionsComboBox.setBounds(SizeManager.filterCourseBounds);
+		gradeOptionsComboBox.setFont(FontManager.fontFilter);
 		gradeOptionsComboBox.setRenderer(categoryComboBoxRenderer);
 		add(gradeOptionsComboBox);
 
