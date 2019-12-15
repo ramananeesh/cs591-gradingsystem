ec31d2fcd1b5cdb7082459d658c6632ad6b46220
Aneesh Raman <aneeshraman97@gmail.com>
2019-12-15 16:57:50 -0500

    Fixed a bug in finalize grades

--- src/view/FinializePanel.java
+++ src/view/FinializePanel.java
@@ -83,11 +83,12 @@ public class FinializePanel extends JPanel implements Observer {
 		gradeTableScrollPane.setBounds(SizeManager.tableCourseBounds);
 		add(gradeTableScrollPane);
 
-		List<Double> statisticsGrades = new ArrayList<Double>();
-		for (int i = 0; i < gradeTableRowData.length; i++) {
-            statisticsGrades.add(Double.parseDouble((String) gradeTableRowData[i][2]) / 100);
-		}
-		Statistics statistics = new Statistics(statisticsGrades);
+//		List<Double> statisticsGrades = new ArrayList<Double>();
+//		for (int i = 0; i < gradeTableRowData.length; i++) {
+//            statisticsGrades.add(Double.parseDouble((String) gradeTableRowData[i][2]) / 100);
+//		}
+		
+		Statistics statistics = new Statistics(controller.getCurrentCourse().getFinalGradesForStats());
 		// statistics label
 		JLabel statisticsLabel = new JLabel(statistics.toString());
 		statisticsLabel.setBounds(SizeManager.labelGradeStatisticsBounds);
